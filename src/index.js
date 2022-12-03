03a4ff56d562280fa96277477223d94821f029fc
oytunkuday <oytunkuday2001@hotmail.com>
2022-12-03 03:55:45 +0300

    small update

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ const express = require("express");
 const app = require("./api/app");
 const bodyParser = require("body-parser");
 
-
 const port = 3000;
 
-mongoose
-  .connect(
-    "mongodb+srv://oytunkuday:Q9kQ1Anrp1Nj5zYD@simpleservicez.yptjr5t.mongodb.net/?retryWrites=true&w=majority"
-  )
-  .then(() => console.log("DB Connection established!"));
+async function db() {
+  await mongoose
+    .connect(
+      "mongodb+srv://oytunkuday:Q9kQ1Anrp1Nj5zYD@simpleservicez.yptjr5t.mongodb.net/?retryWrites=true&w=majority"
+    )
+    .then(() => console.log("DB Connection established!"));
+}
 
+db();
 app.listen(port, () => {
   console.log(`App is running on port ${port}`);
 });
